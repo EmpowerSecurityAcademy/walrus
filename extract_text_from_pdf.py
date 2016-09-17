6162b7023be7f22f561ad120c6958c1555e760eb
sheltowt <sheltowt@gmail.com>
2016-09-17 16:08:24 -0400

    tech bro being a douche

--- extract_text_from_pdf.py
+++ extract_text_from_pdf.py
@@ -1,14 +1,37 @@
 #!python
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
+from cStringIO import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
 
-def get_toc(pdf_path):
-    infile = open(pdf_path, 'rb')
-    parser = PDFParser(infile)
-    document = PDFDocument(parser)
+def main():
+    text = convert("./tmp/document.pdf")
+    print(text)
+    text_file = open("./tmp/pdf_text.txt", "w")
+    text_file.write(text)
+    text_file.close()
 
-    toc = list()
-    for (level,title,dest,a,structelem) in document.get_outlines():
-        toc.append((level, title))
+def convert(fname, pages=None):
+    if not pages:
+        pagenums = set()
+    else:
+        pagenums = set(pages)
 
-    return toc
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    infile = file(fname, 'rb')
+    for page in PDFPage.get_pages(infile, pagenums):
+        interpreter.process_page(page)
+    infile.close()
+    converter.close()
+    text = output.getvalue()
+    output.close
+    return text
+
+
+if __name__ == "__main__":
+    main()
